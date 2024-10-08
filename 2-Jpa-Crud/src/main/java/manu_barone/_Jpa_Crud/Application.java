d1113f06f2010afc1dcde4738e3e0e2bb9dadab2
Manuel Barone <baronemanuel05@gmail.com>
2024-10-08 14:11:26 +0200

    nope

--- 2-Jpa-Crud/src/main/java/manu_barone/_Jpa_Crud/Application.java
+++
@@ -1,8 +0,0 @@
-package manu_barone._Jpa_Crud;
-
-public class Application {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- 2.Jpa-Crud/src/main/java/entities/TIpoEvento.java
+++
@@ -1,5 +0,0 @@
-package entities;
-
-public enum TIpoEvento {
-	PUBBLICO,PRIVATO;
-}
